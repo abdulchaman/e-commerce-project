b80db9cf97dd7f7d9f6e01263bcbde310847b3c3
abdulchaman <abdulchaman123@gmail.com>
2023-06-18 07:37:44 -0700

    project restart

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React,{useState, useEffect} from "react";
+import {commerce} from "./lib/commerce";
+
+import { Products, Navbar } from "./components";
+
+const App = ()=>{
+    const [products,setProducts] = useState([]);
+
+    const fetchProducts = async()=>{
+        const {data} = await commerce.products.list();
+        setProducts(data);
+    }
+    useEffect(()=>{
+        fetchProducts();
+    },[]);
+    return(
+        <div>
+            <Navbar></Navbar>
+           <Products products={products}></Products>
+        </div>
+    )
+}
+export default App;
+
